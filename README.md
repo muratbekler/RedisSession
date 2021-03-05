fc2019fc5ed57a37f1beedc281409d2e2b3a960c
Murat BEKLER <murat@kodmatik.com>
2021-03-06 00:50:51 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# RedisSession
+Configuration and use of ASP.NET Session State Provider for Redis
+
+# Documentation
+The documentation of the project is <a href="https://kodmatik.com/asp-net-session-state-provider-with-azure-redis-cache/">here</a>.
---
+++ RedisSession/BusinessLayer/ApplicationSession.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RedisSession.BusinessLayer
+{
+    public static class ApplicationSession
+    {
+        private static string _userName = "userName";
+        private static string _email = "email";
+        private static string _loginDate = "loginDate";
+
+        public static void Init(string userName, string email)
+        {
+            Set(_userName, userName);
+            Set(_email, email);
+            Set(_loginDate, DateTime.Now);
+        }
+
+        public static string UserName
+        {
+            get
+            {
+                return Get<string>(_userName);
+            }
+            set
+            {
+                Set(_userName, value);
+            }
+        }
+
+        public static string Email
+        {
+            get
+            {
+                return Get<string>(_email);
+            }
+            set
+            {
+                Set(_email, value);
+            }
+        }
+
+        public static DateTime StartDate
+        {
+            get
+            {
+                return Get<DateTime>(_loginDate);
+            }
+            set
+            {
+                Set(_loginDate, value);
+            }
+        }
+
+        private static T Get<T>(string key)
+        {
+            return (T) HttpContext.Current.Session[key];
+        }
+
+        private static void Set(string key, object value)
+        {
+            HttpContext.Current.Session[key] = value;
+        }
+    }
+}
